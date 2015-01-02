2a2871ddee577bd14f172dc873dd95252bd8e054
Chris Mungall <cjm@berkeleybop.org>
2015-01-01 16:59:22 -0800

    new

--- _posts/add-tracker-links.pl
+++ _posts/add-tracker-links.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl -pi -n
 
 if (!$done) {
-    s@ \#(\d+)@ [issue $1](https://github.com/obophenotype/uberon/issues/$1)@g;
+    s@ \#(\d+)@ [$1](https://github.com/obophenotype/uberon/issues/$1)@g;
+    s@\(\#(\d+)@\([$1](https://github.com/obophenotype/uberon/issues/$1)@g;
 }
 
 $done = 1 if m@^\## Original Ontology@;
